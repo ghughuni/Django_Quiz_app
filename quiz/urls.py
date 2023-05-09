06306a94d9bb1745a5dabfce93ced32be8bb9214
main <gkikvidze91@gmail.com>
2023-05-09 17:05:48 +0400

    add contact.html file

--- quiz/urls.py
+++ quiz/urls.py
@@ -2,24 +2,25 @@ from django.urls import path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from .views import (
-			index, 
-			register, 
-			loginView, 
-			logout_view,
-			HomeUser, 
-			action,
-			panel,
-            )
+    index,
+    register,
+    loginView,
+    logout_view,
+    HomeUser,
+    action,
+    panel,
+    contact,
+)
 
 urlpatterns = [
-	path('', index, name='index'),
-	path('HomeUser/', HomeUser, name='HomeUser'),
-	path('login/', loginView, name='login'),
-	path('logout_view/', logout_view, name='logout_view'),
-	path('register/', register, name='register'),
-	path('panel/', panel, name='panel'),
-	path('action/', action, name='action'),
-    
+    path('', index, name='index'),
+    path('HomeUser/', HomeUser, name='HomeUser'),
+    path('login/', loginView, name='login'),
+    path('logout_view/', logout_view, name='logout_view'),
+    path('register/', register, name='register'),
+    path('panel/', panel, name='panel'),
+    path('action/', action, name='action'),
+    path('contact/', contact, name='contact'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
